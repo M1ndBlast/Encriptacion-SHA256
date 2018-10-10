d6650abb52e7e05a436abb8c4c7aa5dd727b9b90
M1ndBlast <gustavopdzz0@gmail.com>
2018-10-09 19:55:18 -0500

    impletacion de conexion

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/encriptacion' () =>
+{
+    var con = mysql.createConnection(
+    {
+        host: "1192.168.21.36",
+        user: "root",
+        password: "Ilovebr3ad",
+        database: "aes"
+    })
+
+    con.connect(function(err)
+    {
+        if (err) throw err;
+        con.query("SELECT * FROM customers", function (err, result, fields)
+        {
+            if (err) throw err;
+            console.log(result);
+        });
+    });
+})
+
 module.exports = router;
