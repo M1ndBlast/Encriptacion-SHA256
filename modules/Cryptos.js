0bd7f64eefc0380dc8892fed8ed509f47ec60d65
Cracell Craft Hunter <cracellch@gmail.com>
2018-10-16 19:20:15 -0500

    Decrypt

--- modules/Cryptos.js
+++ modules/Cryptos.js
@@ -83,7 +83,76 @@ var Cryptos =
 
     decrypt: (encrypted, key, algorthim) =>
     {
-        if (algorthim != Cryptos.algorthim.AES128 && algorthim != Cryptos.algorthim.AES192 && algorthim != Cryptos.algorthim.AES256) return null
+      if (algorthim != Cryptos.algorthim.AES128 && algorthim != Cryptos.algorthim.AES192 && algorthim != Cryptos.algorthim.AES256) return null
+      else
+      {
+          if(algorthim = Cryptos.algorthim.AES128){
+            let  iv = 'Nonosmatesjimmyp'
+            let keyto = crypto.createHash('sha256').update(String(key)).digest('base64').substr(0, 16);
+
+            console.log('> Original es:')
+            console.log(text)
+            console.log('\n')
+            /*
+            key = Cryptos.setAutoPadding(key, bytes)*/
+
+            const cipher = crypto.createDescipheriv(algorthim, keyto, iv)
+
+            let encrypted = cipher.update(text, 'hex', 'utf8')
+            encrypted += cipher.final('utf8')
+
+            console.log('> Descriptado es:')
+            console.log(Desencrypted)
+            console.log('\n')
+
+            return encrypted
+          }
+          if(algorthim = Cryptos.algorthim.AES192){
+            let  iv = 'Nonosmatesjimmyp'
+            let keyto = crypto.createHash('sha256').update(String(key)).digest('base64').substr(0, 24);
+
+            console.log('> Original es:')
+            console.log(text)
+            console.log('\n')
+            /*
+            key = Cryptos.setAutoPadding(key, bytes)*/
+
+            const cipher = crypto.createDescipheriv(algorthim, keyto, iv)
+
+            let encrypted = cipher.update(text, 'hex', 'utf8')
+            encrypted += cipher.final('utf8')
+
+            console.log('> Descriptado es:')
+            console.log(Desencrypted)
+            console.log('\n')
+
+            return encrypted
+          }else{
+            let  iv = 'Nonosmatesjimmyp'
+            let keyto = crypto.createHash('sha256').update(String(key)).digest('base64').substr(0, 32);
+
+            console.log('> Original es:')
+            console.log(text)
+            console.log('\n')
+            /*
+            key = Cryptos.setAutoPadding(key, bytes)*/
+
+            const cipher = crypto.createDescipheriv(algorthim, keyto, iv)
+
+            let encrypted = cipher.update(text, 'hex', 'utf8')
+            encrypted += cipher.final('utf8')
+
+            console.log('> Descriptado es:')
+            console.log(Desencrypted)
+            console.log('\n')
+
+            return encrypted
+          }
+
+
+      }
+  },
+        /*if (algorthim != Cryptos.algorthim.AES128 && algorthim != Cryptos.algorthim.AES192 && algorthim != Cryptos.algorthim.AES256) return null
         else
         {
             console.log('> Encriptado es 1:')
@@ -105,8 +174,7 @@ console.log('jala x4')
             console.log('> Desencriptado es:')
             console.log(decrypted)
             console.log('\n')
-        }
-    },
+        }*/
 
     /*
         Important: Us setAutoPadding need stay before of crypto.createCipheriv()
